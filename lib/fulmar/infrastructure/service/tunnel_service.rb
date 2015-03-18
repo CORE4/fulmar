52c3533272fde704157f42da6a34b3d5bdd2c322
Gerrit Visscher <g.visscher@core4.de>
2015-03-18 14:49:12 +0100

    [FEATURE] Add tunnel service

---
+++ lib/fulmar/infrastructure/service/tunnel_service.rb
@@ -0,0 +1,48 @@
+require 'socket'
+
+module Fulmar
+  module Infrastructure
+    module Service
+      class TunnelService
+        attr_reader :host, :remote_port, :local_port
+
+        def initialize(host, port)
+          @host = host
+          @remote_port = port
+          @local_port = 0
+          @tunnel_pid = 0
+        end
+
+        def open
+          @local_port = free_port
+          @tunnel_pid = Process.spawn "ssh #{@host} -L #{@local_port}:localhost:#{@remote_port} -N"
+        end
+
+        def close
+          Process.kill 'TERM', @tunnel_pid if @tunnel_pid > 0
+          @local_port = 0
+          @tunnel_pid = 0
+        end
+
+        def open?
+          @tunnel_pid > 0
+        end
+
+        def free_port
+          port = 60000
+          begin
+            1000.times do
+              socket = TCPSocket.new('localhost', port)
+              socket.close
+              port += 1
+            end
+          rescue Errno::ECONNREFUSED
+            return port
+          end
+          fail 'Cannot find an open local port'
+          0
+        end
+      end
+    end
+  end
+end
