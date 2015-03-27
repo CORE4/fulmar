ebd34f42c5dbc41b20474a581c8f2b0470281def
Gerrit Visscher <g.visscher@core4.de>
2015-03-27 09:26:13 +0100

    [TASK] Add reminder to think about what I did :)

--- lib/fulmar/domain/service/configuration_service.rb
+++ lib/fulmar/domain/service/configuration_service.rb
@@ -5,6 +5,7 @@ module Fulmar
     module Service
       # Loads and prepares the configuration from the yaml file
       # TODO: Clone target configuration when used as a parameter to another service so an environment change won't affect it
+      # Not Sure if that is actually a god idea
       class ConfigurationService
         FULMAR_FILE = 'Fulmarfile'
         FULMAR_CONFIGURATION = 'FulmarConfiguration'
