39c007f54f4340ae45d3bd749fe09d4b29c559cd
Gerrit Visscher <g.visscher@core4.de>
2015-03-31 12:51:32 +0200

    [BUGFIX] Supply ssh user to remote shell

--- lib/fulmar/service/helper/common_helper.rb
+++ lib/fulmar/service/helper/common_helper.rb
@@ -25,7 +25,7 @@ module Fulmar
           end
 
           def remote_shell
-            storage['remote_shell'] ||= new_shell(configuration[:remote_path], configuration[:hostname])
+            storage['remote_shell'] ||= new_shell(configuration[:remote_path], (configuration[:user] ? configuration[:user]+'@' : '')+configuration[:hostname])
           end
 
           def file_sync
