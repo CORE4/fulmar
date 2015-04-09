7db2f196a5ee210e2e96acf08c0cfa0b8de7a17c
Gerrit Visscher <g.visscher@core4.de>
2015-04-09 16:27:53 +0200

    [BUGFIX] Set consistent default parameters

--- lib/fulmar/infrastructure/service/composer_service.rb
+++ lib/fulmar/infrastructure/service/composer_service.rb
@@ -3,12 +3,14 @@ module Fulmar
     module Service
       # Provides access to composer
       class ComposerService
+        DEFAULT_PARAMS = ['--no-dev']
+
         def initialize(shell, custom_path = '/usr/bin/env composer')
           @local_shell = shell
           @path = custom_path
         end
 
-        def execute(command, arguments = %w(--no-dev -q))
+        def execute(command, arguments = DEFAULT_PARAMS)
           @local_shell.run "#{@path} #{command} #{arguments.join(' ')}"
         end
       end
--- lib/fulmar/service/helper/common_helper.rb
+++ lib/fulmar/service/helper/common_helper.rb
@@ -17,7 +17,7 @@ module Fulmar
             (@_config_service ||= Fulmar::Domain::Service::ConfigurationService.instance)
           end
 
-          def composer(command, arguments = ['--no-dev'])
+          def composer(command, arguments = Fulmar::Infrastructure::Service::ComposerService.DEFAULT_PARAMS)
             (storage['composer'] ||= Fulmar::Infrastructure::Service::ComposerService.new(local_shell)).execute(command, arguments)
           end
 
