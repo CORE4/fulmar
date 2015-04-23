40238a4ce48ce94a899521c744c7068b5ad16325
Gerrit Visscher <g.visscher@core4.de>
2015-04-23 16:47:12 +0200

    [FEATURE] Add more configuration tests

--- lib/fulmar/domain/service/config_test_service.rb
+++ lib/fulmar/domain/service/config_test_service.rb
@@ -3,7 +3,7 @@ require 'erb'
 module Fulmar
   module Domain
     module Service
-      # Renders templates of config files
+      # Tests the configuration
       class ConfigTestService
 
         def initialize(config)
@@ -12,6 +12,7 @@ module Fulmar
           @config.reset
         end
 
+        # Runs all methods beginning with test_ and returns the report
         def run
           @report = []
           tests = self.methods.select { |name| name.to_s[0, 5] == 'test_' }
@@ -47,7 +48,84 @@ module Fulmar
           end
         end
 
+        def test_required_hostnames
+          types = %i(rsync rsync_with_version maria)
+          @config.each do |env, target, data|
+            if types.include? data[:type] and data[:hostname].blank?
+              @report << {
+                message: "#{env}:#{target} requires a hostname (#{data[:hostname]})",
+                severity: :error
+              }
+            end
+          end
+        end
+
+        def test_vhost_template_is_set
+          vhost_template = false
+
+          @config.each do |env, target, data|
+            unless data[:vhost_template].blank?
+              vhost_template = true
+            end
+          end
+
+          add_report(
+            'The configuration uses the vhost feature but misses a valid configuration for it (missing :vhost_template)',
+            :warning
+          ) if @config.feature?(:vhost) && !vhost_template
+        end
+
+        # Run simple test which only require one configuration
+        def test_simple_tests
+          @config.each do |env, target, data|
+            tests = self.methods.select { |name| name.to_s[0, 12] == 'simple_test_' }
+            tests.each do |test|
+              self.send(test, env, target, data)
+            end
+          end
+        end
+
+        def simple_test_local_path_exists(env, target, data)
+          unless File.exist? data[:local_path]
+            @report << {
+              message: "#{env}:#{target} has no valid local_path (#{data[:local_path]})",
+              severity: :warning
+            }
+          end
+        end
+
+        def simple_test_mariadb_feature_is_set(env, target, data)
+          if data[:type] == :maria && !@config.feature?(:database)
+            @report << {
+              message: "#{env}:#{target} uses mysql/mariadb but your config is missing the database feature",
+              severity: :notice
+            }
+          end
+        end
+
+        def simple_test_vhost_feature_is_set(env, target, data)
+          if data[:vhost_template] && !@config.feature?(:vhost)
+            @report << {
+              message: "#{env}:#{target} refers to a vhost_template but your config is missing the vhost feature",
+              severity: :warning
+            }
+          end
+        end
+
+        def simple_test_maria_db_config_exists(env, target, data)
+          if data[:type] == :maria && data[:maria][:database].blank?
+            add_report "#{env}:#{target} is missing a database name in maria:database", :error
+          end
+        end
+
         protected
+
+        def add_report(message, severity)
+          @report << {
+            message: message,
+            severity: severity
+          }
+        end
 
         def ssh_hostnames
           `grep -E '^Host [^ *]+$' ~/.ssh/config | sort | uniq | cut -d ' ' -f 2`.split("\n")
