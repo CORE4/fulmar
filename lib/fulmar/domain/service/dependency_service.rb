710f517e949fce1602139023d007525e0860d10c
Jonas Siegl <j.siegl@core4.de>
2015-08-11 17:05:09 +0200

    [FEATURE] Checkout a revision during the dependency setup

--- lib/fulmar/domain/service/dependency_service.rb
+++ lib/fulmar/domain/service/dependency_service.rb
@@ -18,6 +18,9 @@ module Fulmar
             shell.last_error.each do |line|
               puts line unless line.include? 'already exists and is not an empty directory'
             end
+
+            git = Rugged::Repository.new(data[:path])
+            checkout(git, data)
           end
         end
 
@@ -26,16 +29,7 @@ module Fulmar
             next unless data[:type].blank? || data[:type] == 'git'
             git = Rugged::Repository.new(data[:path])
 
-            # Switch to the configured branch/tag/commit
-            if git.branches.select { |b| b.name.split('/').last == data[:ref] }.any?
-              checkout_branch(git, data[:ref])
-            elsif git.tags.map(&:name).include?(data[:ref])
-              git.checkout("refs/tags/#{data[:ref]}")
-            elsif data[:ref].match(/^[a-zA-Z0-9]{40}$/) && git.exists?(data[:ref])
-              git.checkout(data[:ref])
-            else
-              fail "Cannot find ref #{data[:ref]} in repo #{data[:path]}"
-            end
+            checkout(git, data)
 
             # Pull
             shell = Fulmar::Infrastructure::Service::ShellService.new data[:path]
@@ -46,6 +40,19 @@ module Fulmar
         end
 
         protected
+
+        def checkout(git, data)
+          # Switch to the configured branch/tag/commit
+          if git.branches.select { |b| b.name.split('/').last == data[:ref] }.any?
+            checkout_branch(git, data[:ref])
+          elsif git.tags.map(&:name).include?(data[:ref])
+            git.checkout("refs/tags/#{data[:ref]}")
+          elsif data[:ref].match(/^[a-zA-Z0-9]{40}$/) && git.exists?(data[:ref])
+            git.checkout(data[:ref])
+          else
+            fail "Cannot find ref #{data[:ref]} in repo #{data[:path]}"
+          end
+        end
 
         def checkout_branch(git, branch, remote = 'origin')
           if git.branches.collect(&:name).include? branch
