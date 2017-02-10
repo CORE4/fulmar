6b2fb25496c1ddf802278617a48c418a731f6756
Gerrit Visscher <g.visscher@core4.de>
2017-02-10 16:30:24 +0100

    [BUGFIX] Fix variable name

--- lib/fulmar/domain/service/config_tests/hosts.rb
+++ lib/fulmar/domain/service/config_tests/hosts.rb
@@ -11,13 +11,13 @@ target_test 'hostnames in ssh config' do |config|
   next if config[:hostname].blank?
 
   unless ssh_hostnames.include? config[:hostname]
-    {severity: :info, message: "config has a hostname (#{data[:hostname]}) which is not in your ssh config"}
+    {severity: :info, message: "config has a hostname (#{config[:hostname]}) which is not in your ssh config"}
   end
 end
 
 target_test 'required hostnames' do |config|
   types = %i(rsync rsync_with_version maria)
   if types.include?(config[:type]) && config[:hostname].blank?
-    next {severity: :error, message: "config requires a hostname (#{data[:hostname]})"}
+    next {severity: :error, message: "config requires a hostname (#{config[:hostname]})"}
   end
 end
